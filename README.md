bee0ed466e2df3b6ab853557ce9637db927edbce
Rinigus <rinigus.git@gmail.com>
2017-05-03 19:41:30 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# mapnik-styles-sqlite
+Mapnik Styles for use with SQLite database
+==========================================
+
+This repository is intended for a collection of Mapnik styles that
+work with the same database schema. The corresponding importer is in
+import subfolder of this repository.
+
+At present, only OSM Bright style is available. The style is generated
+on the basis of OSM Bright (most of the organization of the style and
+scripts) and OSM Bright TM2 (style adopted).
+
+OSM Bright TM2: https://github.com/mapbox/mapbox-studio-osm-bright.tm2
+OSM Bright: https://github.com/mapbox/osm-bright
+
+Styles are writen in Carto styling language (http://github.com/mapbox/carto).
+
+Import
+======
+
+Import script allows you to prepare SQLite database from OpenStreetMap
+PBF dataset. For that, you would have to have ogr2ogr and spatialite
+available. See `import_pbf.sh` for details.
+
+
+OSM Bright SQLite
+=================
+
+OSM Bright SQLite is based on OSM Bright Mapnik style (version TM2)
+which is tailored to be used by SQLite database backend.
+
+![screenshot](https://raw.github.com/rinigus/mapnik-styles-sqlite/master/preview.png)
+
+
+### Acknowledgments
+
+Importer script is based on a blog post
+http://www.ram-solutions.nl/2015/converting-openstreetmap-data-to-sqlite-and-render-it-using-the-osmbright-style
+and the SQL script provided by the author of the post.
+
+The approach described in that post was taken as a basis and tuned
+further to provide complete solution.
---
+++ configure.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from os import path, getcwd
+from collections import defaultdict
+config = defaultdict(defaultdict)
+
+config["importer"] = "spatialite"
+
+# The name given to the style. This is the name it will have in the TileMill
+# project list, and a sanitized version will be used as the directory name
+# in which the project is stored
+config["name"] = "OSM Bright SQLite"
+
+# The absolute path to your MapBox projects directory. You should 
+# not need to change this unless you have configured TileMill specially
+config["path"] = path.expanduser("./install")
+
+# SQLite connection setup 
+config["sqlite"]["metadata"] = "mapnik_metadata"
+config["sqlite"]["file"]     = "country/mapnik.sqlite"
+config["sqlite"]["initdb"] = "PRAGMA cache_size = -20480;" # SELECT load_extension('mod_spatialite');"
+config["sqlite"]["geometry_field"] = "geometry"
+config["sqlite"]["wkb_format"] = "generic"
+config["sqlite"]["wkb_format"] = "twkb"
+config["sqlite"]["use_spatial_index"] = "true"
+config["sqlite"]["type"] = "sqlite"
+
+# Increase performance if you are only rendering a particular area by
+# specifying a bounding box to restrict queries. Format is "XMIN,YMIN,XMAX,YMAX"
+# in the same units as the database (probably spherical mercator meters). The
+# whole world is "-20037508.34 -20037508.34 20037508.34 20037508.34".
+# Leave blank to let Mapnik estimate.
+#config["sqlite"]["extent"] = "-20037508.34,-20037508.34,20037508.34,20037508.34"
+
+# Land shapefiles required for the style. If you have already downloaded
+# these or wish to use different versions, specify their paths here.
+# You will need to unzip these files before running make.py
+# These OSM land shapefiles are updated daily and can be downloaded at: 
+# - http://data.openstreetmapdata.com/simplified-land-polygons-complete-3857.zip
+# - http://data.openstreetmapdata.com/land-polygons-split-3857.zip
+
+config["land-high"] = "global/land-polygons-split-3857/land_polygons.shp"
+config["land-low"] = "global/simplified-land-polygons-complete-3857/simplified_land_polygons.shp"
